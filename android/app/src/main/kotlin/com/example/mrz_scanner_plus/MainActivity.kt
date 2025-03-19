36a6d07c0a54b9d980907c266c533405dd0b0ff6
Thmoas Ber* <87644022+fastcode555@users.noreply.github.com>
2025-03-19 10:50:29 +0700

    变成插件的方式

--- android/app/src/main/kotlin/com/example/mrz_scanner_plus/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mrz_scanner_plus
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ lib/mrz_scanner_plus.dart
@@ -0,0 +1,4 @@
+export 'src/camera_view.dart';
+export 'src/mask_painter.dart';
+export 'src/mrz_extension.dart';
+export 'src/mrz_helper.dart';
